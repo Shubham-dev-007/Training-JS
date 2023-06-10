9369626e84d32d11a271f87ae6b5ab03516d11c2
Shubham-dev-007 <136158313+Shubham-dev-007@users.noreply.github.com>
2023-06-10 14:05:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// synchronous execution
+
+// var parent = document.querySelector('body');
+// var child = document.createElement('div');
+// parent.appendChild(child);
+// // child.innerHTML = 'this is division tag!';
+// var grandChild = document.createElement('h1');
+// child.appendChild(grandChild);
+// grandChild.innerHTML = 'this is heading tag!';
+// var para = document.createElement('p');
+// document.getElementsByTagName('div')[0].children[0] = para;
+
+function myFunction() {
+    alert('he;loworlscdch')
+}
+
+
+
